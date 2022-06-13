a739cba181ef89811af2b1ac09a658690dd322e7
Nicholas A Perry <nicholasperrypro@gmail.com>
2022-06-13 18:28:43 -0400

    init commit, finished episode 8/8 of the dotnet maui tutorial for beginners

---
+++ MauiApp1/ViewModels/DetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiApp1.ViewModels
+{
+    [QueryProperty("Item", "Item")]
+	public partial class DetailViewModel : ObservableObject
+	{
+		public DetailViewModel()
+		{
+		}
+
+		[ObservableProperty]
+		string item;
+
+        [ICommand]
+		async Task GoBack()
+        {
+			await Shell.Current.GoToAsync("..");
+        }
+	}
+}
+
---
+++ MauiApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiApp1.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        IConnectivity _connectivity;
+        public MainViewModel(IConnectivity connectivity)
+        {
+            Items = new ObservableCollection<string>();
+            _connectivity = connectivity;
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [ICommand]
+        async void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+            {
+                return;
+            }
+            if (_connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("F*CK", "You ain't got no internet cus!!", "Shit, aii.");
+                return;
+            }
+            
+
+            items.Add(Text);
+            Text = string.Empty;
+        }
+        [ICommand]
+        void Delete(string item)
+        {
+            if (Items.Contains(item))
+            {
+                Items.Remove(item);
+            }
+        }
+        [ICommand]
+        async Task Tap(string item)
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Item={item}");
+            //await Shell.Current.GoToAsync($"{nameof(DetailPage)}",
+            //    parameters: new Dictionary<string, object>
+            //    {
+            //        {"Item", item }
+            //    });
+        }
+    }
+}
+
